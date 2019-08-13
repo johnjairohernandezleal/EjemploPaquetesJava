ce322ef53de2015ef83e03ef9f27c8172db1c0d9
unknown <john_0112@hotmail.com>
2019-08-13 01:21:07 -0500

    first commit

---
+++ src/EjemploPaquetes.java
@@ -0,0 +1,15 @@
+import com.gm.Utileria;
+import static com.gm.Utileria.imprimir;
+// import metodos estaticos
+
+public class EjemploPaquetes {
+    public static void main(String[] args) {
+      Utileria.imprimir("hola");
+      com.gm.Utileria.imprimir("como estas"); // sin hacer uso del import
+      imprimir("bn");
+
+
+
+
+    }
+}
